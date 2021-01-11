fa431f40e6eb1db6a5f89ed29f21bd39f71a7f9f
Aron Truu <saararon@gmail.com>
2021-01-11 09:22:06 +0200

    exercise 1.3

--- src/index.js
+++ src/index.js
@@ -38,6 +38,31 @@ const Total = (props) => {
 }
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1.name} p2={part2.name} p3={part3.name} e1={part1.exercises} e2={part2.exercises} e3={part3.exercises}/>
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises}/>
+    </div>
+  )
+}
+
+/*
+const App = () => {
   // const-definitions
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -55,35 +80,7 @@ const App = () => {
     </div>
   )
 }
-
-
-
-/*
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
 */
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
